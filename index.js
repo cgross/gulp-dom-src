ee0bcb6d6c82265475da638966f531b211135424
Paul Salaets <psalaets@gmail.com>
2015-04-10 22:11:29 -0400

    add duplex capability so html can be piped in

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var vinylFs = require('vinyl-fs');
 var cheerio = require('cheerio');
+var through = require('through2');
+var vinylFile = require('vinyl-file');
+var async = require('async');
+var extend = require('extend');
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function(config){
+module.exports = domSrc;
 
+function domSrc(config){
     var html = fs.readFileSync(path.join(process.cwd(),config.file),'utf8');
+    var files = findFilenames(html, config);
+
+    return vinylFs.src(files,config.options || {});
+}
+
+function findFilenames(html, config) {
     var $ = cheerio.load(html);
 
-    var files = $(config.selector).map(function(i,elem){
+    return $(config.selector).map(function(i,elem){
         return $(elem).attr(config.attribute);
     }).toArray().filter(function(item){
         return (item !== undefined && item.substring(0,4) !== 'http' && item.substring(0,2) !== '//');
@@ -16,6 +27,34 @@ module.exports = function(config){
         var cwd = config.cwd ? config.cwd : path.dirname(config.file);
         return path.join(cwd,item);
     });
+}
 
-    return vinylFs.src(files,config.options || {});
+domSrc.duplex = function(config) {
+    return through.obj(findAndReadFiles);
+
+    function findAndReadFiles(htmlFile, encoding, throughCallback) {
+        // copy config and set config.file to current html file
+        // so cwd is resolved correctly when scanning html for filenames
+        var configCopy = extend({}, config);
+        configCopy.file = htmlFile.path;
+
+        var html = htmlFile.contents.toString();
+        var filenames = findFilenames(html, configCopy);
+
+        if (filenames.length == 0) {
+            return throughCallback();
+        }
+
+        var throughStream = this;
+        async.eachSeries(filenames, function(filename, eachCallback) {
+            vinylFile.read(filename, function(err, file) {
+                if (err) return eachCallback(err);
+
+                throughStream.push(file);
+                eachCallback();
+            });
+        }, function(err) {
+            throughCallback(err);
+        });
+    }
 };
--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var fs = require('fs');
 var should = require('should');
 var domSrc = require('../index');
 var vfs = require('vinyl-fs');
@@ -12,39 +11,103 @@ var dataWrap = function(fn) {
     };
 };
 
-it('should read the script tags src into the stream', function (done) {
+describe('src (default) mode', function () {
+    it('should read the script tags src into the stream', function (done) {
 
-    var onEnd = function(){
-        buffered.length.should.equal(3);
-        should.exist(buffered[0].stat);
-        buffered[0].path.should.equal(path.resolve('test/fixture/one.js'));
-        buffered[1].path.should.equal(path.resolve('test/fixture/two.js'));
-        buffered[2].path.should.equal(path.resolve('test/fixture/three.js'));
-        done();
-    };
+        var onEnd = function(){
+            buffered.length.should.equal(3);
+            should.exist(buffered[0].stat);
+            buffered[0].path.should.equal(path.resolve('test/fixture/one.js'));
+            buffered[1].path.should.equal(path.resolve('test/fixture/two.js'));
+            buffered[2].path.should.equal(path.resolve('test/fixture/three.js'));
+            done();
+        };
 
-    var stream = domSrc({file:'test/fixture/fixture.html',selector:'script',attribute:'src'});
+        var stream = domSrc({file:'test/fixture/fixture.html',selector:'script',attribute:'src'});
 
-    var buffered = [];
-    var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
-    stream.pipe(bufferStream);
+        var buffered = [];
+        var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
+        stream.pipe(bufferStream);
 
+    });
+
+    it('should use the cwd property correctly.', function (done) {
+
+        var onEnd = function(){
+            buffered.length.should.equal(1);
+            should.exist(buffered[0].stat);
+            buffered[0].path.should.equal(path.resolve('test/fixture/one.js'));
+            done();
+        };
+
+        var stream = domSrc({file:'test/fixture/subdir/subdir.html',selector:'script',attribute:'src',cwd:'test/fixture'});
+
+        var buffered = [];
+        var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
+        stream.pipe(bufferStream);
+
+    });
 });
 
-it('should use the cwd property correctly.', function (done) {
+describe('duplex mode', function () {
+    it('should process html piped into it', function(done) {
 
-    var onEnd = function(){
-        buffered.length.should.equal(1);
-        should.exist(buffered[0].stat);
-        buffered[0].path.should.equal(path.resolve('test/fixture/one.js'));
-        done();
-    };
+        var onEnd = function(){
+            buffered.length.should.equal(3);
+            should.exist(buffered[0].stat);
+            buffered[0].path.should.equal(path.resolve('test/fixture/one.js'));
+            buffered[1].path.should.equal(path.resolve('test/fixture/two.js'));
+            buffered[2].path.should.equal(path.resolve('test/fixture/three.js'));
+            done();
+        };
 
-    var stream = domSrc({file:'test/fixture/subdir/subdir.html',selector:'script',attribute:'src',cwd:'test/fixture'});
+        var stream = vfs.src('test/fixture/fixture.html')
+            .pipe(domSrc.duplex({
+                selector:'script',
+                attribute:'src'
+            }));
 
-    var buffered = [];
-    var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
-    stream.pipe(bufferStream);
+        var buffered = [];
+        var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
+        stream.pipe(bufferStream);
+    });
 
+    it('should use the cwd property correctly.', function (done) {
+
+        var onEnd = function(){
+            buffered.length.should.equal(1);
+            should.exist(buffered[0].stat);
+            buffered[0].path.should.equal(path.resolve('test/fixture/one.js'));
+            done();
+        };
+
+        var stream = vfs.src('test/fixture/subdir/subdir.html')
+            .pipe(domSrc.duplex({
+                selector:'script',
+                attribute:'src',
+                cwd: 'test/fixture'
+            }));
+
+        var buffered = [];
+        var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
+        stream.pipe(bufferStream);
+    });
+
+    it('should still work when finding nothing in html', function(done) {
+
+        var onEnd = function(){
+            buffered.length.should.equal(0);
+            done();
+        };
+
+        var stream = vfs.src('test/fixture/fixture.html')
+            .pipe(domSrc.duplex({
+                selector:'script',
+                attribute:'wont-find-this-attribute'
+            }));
+
+        var buffered = [];
+        var bufferStream = through.obj(dataWrap(buffered.push.bind(buffered)), onEnd);
+        stream.pipe(bufferStream);
+    });
 });
-
